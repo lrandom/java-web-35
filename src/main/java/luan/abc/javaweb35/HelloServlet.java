6bec099d191c0e9dd3e1a0e67ad37ca2f8318d6d
Nguyễn Thành Luân <luannguyen@prep.vn>
2024-05-27 21:08:12 +0700

    init

---
+++ src/main/java/luan/abc/javaweb35/HelloServlet.java
@@ -0,0 +1,32 @@
+package luan.abc.javaweb35;
+
+import java.io.*;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.*;
+
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello World!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+
+    public void destroy() {
+    }
+}
